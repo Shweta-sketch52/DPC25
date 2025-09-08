a92fec6d503331f88b4a202bcc0317ab1aeb594a
Shweta-sketch52 <birleshweta951@gmail.com>
2025-09-08 23:22:07 +0530

    Create Day25.cpp

---
+++ Day25.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <climits>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+bool isValidBSTHelper(TreeNode* root, long long minVal, long long maxVal) {
+    if (!root) return true;
+    if (root->val <= minVal || root->val >= maxVal) return false;
+    return isValidBSTHelper(root->left, minVal, root->val) &&
+           isValidBSTHelper(root->right, root->val, maxVal);
+}
+
+bool isValidBST(TreeNode* root) {
+    return isValidBSTHelper(root, LLONG_MIN, LLONG_MAX);
+}
+
+TreeNode* buildTree(vector<int>& nodes) {
+    if (nodes.empty() || nodes[0] == -1) return nullptr;
+
+    TreeNode* root = new TreeNode(nodes[0]);
+    queue<TreeNode*> q;
+    q.push(root);
+
+    int i = 1;
+    while (!q.empty() && i < nodes.size()) {
+        TreeNode* curr = q.front();
+        q.pop();
+
+        if (i < nodes.size() && nodes[i] != -1) {
+            curr->left = new TreeNode(nodes[i]);
+            q.push(curr->left);
+        }
+        i++;
+
+        if (i < nodes.size() && nodes[i] != -1) {
+            curr->right = new TreeNode(nodes[i]);
+            q.push(curr->right);
+        }
+        i++;
+    }
+    return root;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of nodes in level-order (use -1 for null): ";
+    cin >> n;
+
+    vector<int> nodes(n);
+    cout << "Enter tree nodes in level-order: ";
+    for (int i = 0; i < n; i++) {
+        cin >> nodes[i];
+    }
+
+    TreeNode* root = buildTree(nodes);
+
+    if (isValidBST(root)) {
+        cout << "Output: true" << endl;
+    } else {
+        cout << "Output: false" << endl;
+    }
+
+    return 0;
+}
